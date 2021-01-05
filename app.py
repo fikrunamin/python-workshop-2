88e9055bf99c418863ca3e2fd560a392a6e76728
Fikrun Amin <fikrun65@gmail.com>
2021-01-05 14:01:46 +0800

    test

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     text = "hello world"
-    # text = nltk.word_tokenize(text)
-    # text = ', '.join(text)
+    text = nltk.word_tokenize(text)
+    text = ', '.join(text)
     return text
 
+
 if __name__ == '__main__':
-   app.run()
+    app.run()
